a4e4c73626cc1708dab43d37714e86766afdd2a1
arshchouhan <arshchouhan004@gmail.com>
2025-05-17 14:43:15 +0530

    Switched to popup mode for Google Sign-In

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,12 +4,7 @@ const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);  // Single route to handle both roles
 router.post('/signup', authController.signup); // Route for user registration
-router.get('/google/callback', authController.googleSignIn);
-router.get('/google', (req, res) => {
-    const redirectUri = 'https://chdpolice-hackathon.vercel.app/auth/google';
-    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=token&scope=email%20profile`;
-    res.redirect(googleAuthUrl);
-}); // Route for Google Sign-In
+router.post('/google', authController.googleSignIn); // Route for Google Sign-In
 router.post('/logout', authController.logout); // Route for logout
 
 module.exports = router;
